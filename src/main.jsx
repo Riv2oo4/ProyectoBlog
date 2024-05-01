620b294e1e455fd8547df02375a0da94cc9ab2f4
Riv2oo4 <riv22500@uvg.edu.gt>
2024-04-30 20:25:02 -0600

    Update main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+
+import ReactDOM from 'react-dom/client';
+import Navegar from './router/index.jsx';
+import './index.css'
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Navegar />);
+
+export default root; 
+
