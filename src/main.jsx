3e96bb0274e9d75dfa4ab7754876a656cd1374fb
Riv2oo4 <riv22500@uvg.edu.gt>
2024-05-06 19:57:00 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import  "./index.css"
 
-import ReactDOM from 'react-dom/client';
-import Navegar from './router/index.jsx';
-import './index.css'
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Navegar />);
-
-export default root; 
-
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
