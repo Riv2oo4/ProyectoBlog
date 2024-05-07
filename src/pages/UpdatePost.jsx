00aadc521073e4656ed811bb964da2071a158741
Riv2oo4 <riv22500@uvg.edu.gt>
2024-05-06 23:39:12 -0600

    Update UpdatePost.jsx

--- src/pages/UpdatePost.jsx
+++ src/pages/UpdatePost.jsx
@@ -17,7 +17,14 @@ function UpdatePost() {
     useEffect(() => {
         const fetchPosts = async () => {
             try {
-                const response = await axios.get("http://localhost:3000/posts");
+                // Obtener el token del almacenamiento local
+                const token = localStorage.getItem('token');
+
+                const response = await axios.get("http://localhost:3000/posts", {
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                });
                 setPosts(response.data);
             } catch (error) {
                 setError("Error al obtener los posts");
@@ -30,7 +37,14 @@ function UpdatePost() {
 
     const handleEditPost = async (postId) => {
         try {
-            const response = await axios.get(`http://localhost:3000/posts/${postId}`);
+            // Obtener el token del almacenamiento local
+            const token = localStorage.getItem('token');
+
+            const response = await axios.get(`http://localhost:3000/posts/${postId}`, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
             setEditingPostId(postId);
             setEditedPost(response.data);
             setIsModalOpen(true); 
@@ -47,7 +61,14 @@ function UpdatePost() {
 
     const handleUpdatePost = async () => {
         try {
-            await axios.put(`http://localhost:3000/posts/${editingPostId}`, editedPost);
+            // Obtener el token del almacenamiento local
+            const token = localStorage.getItem('token');
+
+            await axios.put(`http://localhost:3000/posts/${editingPostId}`, editedPost, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
 
             const updatedPosts = posts.map(post => {
                 if (post.id === editingPostId) {
@@ -77,7 +98,6 @@ function UpdatePost() {
     return (
         <div className="update-container">
             <h1>Blog UFC</h1>
-
             <hr />
             <div className="posts-container">
                 {posts.map(post => (
@@ -94,33 +114,32 @@ function UpdatePost() {
                 ))}
             </div>
             {isModalOpen && (
-    <div className="modal">
-        <div className="modal-content">
-            <span className="close" onClick={closeModal}>&times;</span>
-            <h2>Editar Post</h2>
-            <form>
-                <div className="form-group">
-                    <label htmlFor="editedTitle">Título</label>
-                    <input type="text" id="editedTitle" name="title" value={editedPost.title} onChange={handleInputChange} />
+                <div className="modal">
+                    <div className="modal-content">
+                        <span className="close" onClick={closeModal}>&times;</span>
+                        <h2>Editar Post</h2>
+                        <form>
+                            <div className="form-group">
+                                <label htmlFor="editedTitle">Título</label>
+                                <input type="text" id="editedTitle" name="title" value={editedPost.title} onChange={handleInputChange} />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="editedContent">Contenido</label>
+                                <textarea id="editedContent" name="content" rows="3" value={editedPost.content} onChange={handleInputChange}></textarea>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="editedResult">Resultado</label>
+                                <input type="text" id="editedResult" name="result" value={editedPost.result} onChange={handleInputChange} />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="editedWinnerImageUrl">URL de la imagen del ganador</label>
+                                <input type="text" id="editedWinnerImageUrl" name="winnerImageUrl" value={editedPost.winnerImageUrl} onChange={handleInputChange} />
+                            </div>
+                            <button className="update-button" onClick={handleUpdatePost}>Actualizar</button>
+                        </form>
+                    </div>
                 </div>
-                <div className="form-group">
-                    <label htmlFor="editedContent">Contenido</label>
-                    <textarea id="editedContent" name="content" rows="3" value={editedPost.content} onChange={handleInputChange}></textarea>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="editedResult">Resultado</label>
-                    <input type="text" id="editedResult" name="result" value={editedPost.result} onChange={handleInputChange} />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="editedWinnerImageUrl">URL de la imagen del ganador</label>
-                    <input type="text" id="editedWinnerImageUrl" name="winnerImageUrl" value={editedPost.winnerImageUrl} onChange={handleInputChange} />
-                </div>
-                <button className="update-button" onClick={handleUpdatePost}>Actualizar</button>
-            </form>
-        </div>
-    </div>
-)}
-
+            )}
             {error && <div className="error">{error}</div>}
         </div>
     );
