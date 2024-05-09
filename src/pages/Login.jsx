2d58c676b23e02bee3e93ff70ac12d0a6a050fbf
Riv2oo4 <riv22500@uvg.edu.gt>
2024-05-08 18:39:08 -0600

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,7 +13,7 @@ function Login() {
 
   const handleLogin = async () => {
     try {
-      const response = await fetch('http://localhost:3000/log', {
+      const response = await fetch('http://22500.arpanetos.lol/log', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
